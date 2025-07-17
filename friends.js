2aaa7747baae284a762538a09acef3f613504781
CuemoDev <devslifer2029@gmail.com>
2025-07-16 23:58:00 -0500

    Update friends.js

--- friends.js
+++ friends.js
@@ -1,1 +1,20 @@
+let friends = ["Player123", "GamerGirl", "NoobSlayer"];
 
+function loadFriends() {
+  const list = document.getElementById("friend-list");
+  list.innerHTML = '';
+  friends.forEach(name => {
+    const li = document.createElement("li");
+    li.textContent = name;
+    list.appendChild(li);
+  });
+}
+
+function addFriend() {
+  const name = document.getElementById("add-friend-name").value;
+  if (name && !friends.includes(name)) {
+    friends.push(name);
+    loadFriends();
+    document.getElementById("add-friend-name").value = '';
+  }
+}
